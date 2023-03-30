aaffa59d34894b9a7f3d449fa722ca916f69c508
Taha Jemli <taha.jemli1@gmail.com>
2023-03-30 17:08:15 +0200

    creating post interfaces

---
+++ src/components/Post/AddPost.js
@@ -0,0 +1,14 @@
+import {useAuthContext} from "../../hooks/useAuthContext";
+import Navbar from "../Common/Navbar/navbar";
+import Footer from "../Common/Footer/footer";
+
+const AddPost = () => {
+    return (
+        <div>
+        <Navbar/>
+        {/*</div><div className="home">*/}
+        <Footer/>
+        </div>
+    )
+}
+export default AddPost;
