283207cb2316294629b0a21cc13eb80cf19086e3
Taha Jemli <taha.jemli1@gmail.com>
2023-04-10 08:48:06 +0200

    Feed fixed

--- src/pages/Feed.js
+++ src/pages/Feed.js
@@ -6,18 +6,25 @@ import styles from "../styles/post.module.css";
 import styled from "../styles/feed.module.css";
 //import { MoreVert } from "@material-ui/icons";
 import profilePicture from '../styles/blank-profile-picture.png';
-
 import likeIcon from "../styles/likeIcon.png";   
 import heartIcon from "../styles/heartIcon.png"; 
+
+import Navbar from "../components/Common/Navbar/navbar";
+import Footer from "../components/Common/Footer/footer";
 
 const Feed =(props) => {
 
     const [error, setError] = useState(null);
     const [isLoading, setIsLoading] = useState(false);
     const [posts, setPosts] = useState([]);
+    const [comments, setComments] = useState([]);
+
     var user = JSON.parse( localStorage.getItem('user') );
     const owner = user._id;
-  
+    const namex = user.email;
+    const [postId, setPostId] = useState('');
+    const [content, setContent] = useState('');
+  // ------------------------------------------------------------------------------------------
     const handleSubmit = async (event) => {
   
       event.preventDefault();
@@ -26,7 +33,7 @@ const Feed =(props) => {
       
       await OnAddPost(formData);
     };
-  
+   // ------------------------------------------------------------------------------------------
     const OnAddPost = (formData) => {
       console.log(user.token)
       setIsLoading(true);
@@ -50,7 +57,7 @@ const Feed =(props) => {
           setIsLoading(false);
         });
     };
-
+ // ------------------------------------------------------------------------------------------
     const fetchPost = async () => {
         // const token = localStorage.getItem('token'); // Récupère le token stocké dans le local storage du navigateur
      
@@ -60,7 +67,7 @@ const Feed =(props) => {
          console.log(response.data);
      
        }
-     
+      // ------------------------------------------------------------------------------------------
        useEffect(()=>{
          const fetchPost = async () => {
            
@@ -73,14 +80,60 @@ const Feed =(props) => {
          }
          fetchPost()
        },[])
+
+
+
+// ------------------------------------------------------------------------------------------
+       const handleCommentSubmit = async (event) => {  
+        event.preventDefault();        
+        await onAddComment(postId,content);
+      };
+ // ------------------------------------------------------------------------------------------
+const onAddComment = async (postId,content) => {
+  console.log(postId)
+  console.log(content)
+  console.log(user.token)
+  setIsLoading(true);
+  axios
+    .post(`/post/addComment`, { postId, content }, {
+        headers: {'Content-Type': 'application/json',
+        'Authorization': `Bearer ${user.token}`},
+    })
+    .then((response) => {
+      console.log("Comment added successfully", response.data);
+      // do something else, such as update the UI
+      setComments([response.data, ...comments]);
+      setIsLoading(false);
+    })
+    .catch((error) => {
+      console.log("Error Commenting ", error);
+      // handle the error, such as displaying an error message to the user
+      setError(error.message);
+      setIsLoading(false);
+    });
+}
+      // ------------------------------------------------------------------------------------------
+       useEffect(()=>{
+         const fetchComment = async () => {
+          
+          const response = await axios.get('/post/getComments', {postId}, {
+           headers: {'Authorization': `Bearer ${user.token}`}})
+           //setUsers(response.data)
+           setComments(response.data);
+           console.log(response.data);
+    
+         }
+         fetchComment()
+       },[])
+
     return (
         <div className={styled.feed}>
+          <Navbar/>
         <div className={styled.feedWrapper}>
-        <div>
-        <form onSubmit={handleSubmit}>
-          <input type="text" name="caption" />
-          <input type="file" name="image" />
-          <button type="submit">Create Post</button>
+        <form onSubmit={handleSubmit} className={styles.post} >
+          <textarea className={styles.postTop} rows="2" placeholder="What's on your mind?" type="text" name="caption" />
+          <input type="file"  name="image" />
+          <button type="submit" className="btn btn-outline-primary ms-1">Create Post</button>
           {isLoading && <p>Loading...</p>}
           {error && <p>{error}</p>}
         </form>
@@ -109,7 +162,7 @@ const Feed =(props) => {
                                 {user.username}
                             </span>
                             <span className={styles.postDate}>
-                                {post.caption}
+                                {post.owner}
                             </span>
                         </div>
                         <div className={styles.postTopRight}>
@@ -138,9 +191,20 @@ const Feed =(props) => {
                         </div>
                     </div>
                 </div>
+                <form onSubmit={handleCommentSubmit} className={styles.comment } >
+          <textarea className={styles.postTop} rows="2" placeholder="What's on your mind?" type="text" name="content" onChange={(e) => {setPostId(post._id), setContent(e.target.value)}}/>
+          <button type="submit" className="btn btn-outline-primary ms-1" >Comment</button>
+          {isLoading && <p>Loading...</p>}
+          {error && <p>{error}</p>}
+        </form>
+        {comments.map((comment) => (
+          <div>
+            <span> {comment.content}</span>
+            </div>
+        ))}
             </div>))}
         </div>
-        </div>
+        <Footer/>
         </div>
     )
 
